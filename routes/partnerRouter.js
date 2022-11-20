d549b06f05d6c07e9bcaa99a6a0a7dc979cab61d
britneyfrizzell@gmail.com <95828527+bfrizzell93@users.noreply.github.com>
2022-11-20 13:58:45 -0600

    workshop 3 authentication admin

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,54 +1,54 @@
 const express = require("express");
 const Partner = require("../models/partner");
 const partnerRouter = express.Router();
-const authenticate = require('../authenticate')
+const authenticate = require("../authenticate");
 
 partnerRouter
   .route("/")
   .get((req, res, next) => {
     Partner.find()
-    .then(partners => {
-      res.statusCode= 200;
-      res.setHeader('Content-Type', 'application/json')
-      res.json(partners)
-    })
-    .catch(err => next(err))
+      .then((partners) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partners);
+      })
+      .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, (req, res, next ) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.create(req.body)
-    .then(partner => {
-      console.log("Partner Created ", partner);
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(partner)
-    })
-    .catch(err => next(err))
+      .then((partner) => {
+        console.log("Partner Created ", partner);
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
   .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /partners");
   })
-  .delete(authenticate.verifyUser, (req, res) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     Partner.deleteMany()
-    .then(response => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(response)
-    })
-    .catch(err => next(err))
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
 partnerRouter
   .route("/:partnerId")
- 
+
   .get((req, res, next) => {
     Partner.findById(req.params.partnerId)
-    .then(partner => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(partner)
-    })
-    .catch(err => next(err))
+      .then((partner) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
 
   .post(authenticate.verifyUser, (req, res) => {
@@ -58,25 +58,28 @@ partnerRouter
     );
   })
 
-  .put(authenticate.verifyUser, (req, res, next) => {
-    Partner.findByIdAndUpdate(req.params.partnerId, {
-      $set: req.body
-    }, {new: true})
-    .then(partner => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(partner)
-    })
-    .catch(err => next(err))
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.findByIdAndUpdate(
+      req.params.partnerId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((partner) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
 
-  .delete(authenticate.verifyUser, (req, res) => {
-    Partner.findByIdAndDelete(req.params.partnerId)
-    .then(response => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    Partner.findByIdAndDelete(req.params.partnerId).then((response) => {
       res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(response)
-    })
+      res.setHeader("Content-type", "application/json");
+      res.json(response);
+    });
   });
 
 module.exports = partnerRouter;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,54 +1,54 @@
 const express = require("express");
 const promotionRouter = express.Router();
-const Promotion = require('../models/promotion')
-const authenticate = require('../authenticate')
+const Promotion = require("../models/promotion");
+const authenticate = require("../authenticate");
 
 promotionRouter
   .route("/")
   .get((req, res, next) => {
     Promotion.find()
-    .then(promotions => {
-      res.statusCode= 200;
-      res.setHeader('Content-Type', 'application/json')
-      res.json(promotions)
-    })
-    .catch(err => next(err))
+      .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(promotions);
+      })
+      .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, (req, res, next ) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.create(req.body)
-    .then(promotion => {
-      console.log("promotion Created ", promotion);
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(promotion)
-    })
-    .catch(err => next(err))
+      .then((promotion) => {
+        console.log("promotion Created ", promotion);
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(promotion);
+      })
+      .catch((err) => next(err));
   })
   .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /promotions");
   })
-  .delete(authenticate.verifyUser, (req, res) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     Promotion.deleteMany()
-    .then(response => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(response)
-    })
-    .catch(err => next(err))
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
 promotionRouter
   .route("/:promotionId")
- 
+
   .get((req, res, next) => {
     Promotion.findById(req.params.promotionId)
-    .then(promotion => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(promotion)
-    })
-    .catch(err => next(err))
+      .then((promotion) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(promotion);
+      })
+      .catch((err) => next(err));
   })
 
   .post(authenticate.verifyUser, (req, res) => {
@@ -58,25 +58,28 @@ promotionRouter
     );
   })
 
-  .put(authenticate.verifyUser, (req, res, next) => {
-    Promotion.findByIdAndUpdate(req.params.promotionId, {
-      $set: req.body
-    }, {new: true})
-    .then(promotion => {
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(promotion)
-    })
-    .catch(err => next(err))
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndUpdate(
+      req.params.promotionId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((promotion) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(promotion);
+      })
+      .catch((err) => next(err));
   })
 
-  .delete(authenticate.verifyUser, (req, res) => {
-    Promotion.findByIdAndDelete(req.params.promotionId)
-    .then(response => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    Promotion.findByIdAndDelete(req.params.promotionId).then((response) => {
       res.statusCode = 200;
-      res.setHeader('Content-type', 'application/json')
-      res.json(response)
-    })
+      res.setHeader("Content-type", "application/json");
+      res.json(response);
+    });
   });
 
 module.exports = promotionRouter;
